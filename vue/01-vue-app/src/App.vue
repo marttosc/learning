3cc4906684ac5260f0fb9f91e10ca4dd6d75a92c
Gustavo Marttos <marttosc@gmail.com>
2016-08-09 12:26:44 -0300

    Creating a component with Vue.

--- vue/01-vue-app/src/App.vue
+++ vue/01-vue-app/src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
+    <menu title="My app"></menu>
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
+import Menu from './Menu.vue'
+
 export default {
+  components: {
+    Menu
+  },
   data () {
     return {
       // note: changing this line won't causes changes
       // with hot-reload because the reloaded component
       // preserves its current state and we are modifying
       // its initial state.
-      msg: 'Hello Vue!'
+      msg: 'Hello, world! Hello, Vue!'
     }
   }
 }
